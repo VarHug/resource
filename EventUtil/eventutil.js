9c11c36734bb7b398be1bfc31f5b1a821e99a3a8
VarHug <490641570@qq.com>
2017-09-28 19:46:18 +0800

    Update EventUtil

--- EventUtil/eventutil.js
+++ EventUtil/eventutil.js
@@ -1,4 +1,4 @@
-var eventUtil = {
+var EventUtil = {
     //添加事件处理程序
     addHandler: function(ele, type, handler) {
         if (ele.addEventListener) {
@@ -42,5 +42,37 @@ var eventUtil = {
         } else {
             event.cancelBubble = true;
         }
+    },
+    //取得相关元素
+    getRelatedTarget: function(event) {
+        if(event.relatedTarget) {
+            return event.relatedTarget;
+        } else if(event.toElement) {
+            return event.toElement;
+        } else if(event.fromElement) {
+            return event.fromElement;
+        } else {
+            return null;
+        }
+    },
+    //获取鼠标按钮button属性(0左、1滚轮、2右)
+    getButton: function(event) {
+        if(document.implementation.hasFeature('MouseEvents', '2.0')) {
+            return event.button;
+        } else {
+            switch(event.button) {
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
     }
-}
+};
